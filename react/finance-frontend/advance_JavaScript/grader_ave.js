c8db94616ca8a5c9899756fe402b9e5aab94d2ea
AtsajanAJ <atsajan08@gmail.com>
2026-01-12 17:03:06 +0700

    fix something

--- react/finance-frontend/advance_JavaScript/grader_ave.js
+++
@@ -1,8 +0,0 @@
-const students = [ {"name" : "Alice", "score" : 63}, {"name" : "Bob", "score" : 80}]
-const totalScore = students.reduce((sum, student) => sum + student.score, 0);
-const numberOfStudents = students.length;
-const averageScore = totalScore / numberOfStudents;
-
-console.log(`Totalscore: ${totalScore}`);
-console.log(`Average Score: ${averageScore}`);
-
